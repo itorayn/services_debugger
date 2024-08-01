e467d78fde457565d4423bcf807aae8cdec51c9f
User <muzyka@protei-lab.ru>
2024-08-01 20:33:24 +0700

    Fix linting

--- app/api/hosts.py
+++ app/api/hosts.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from fastapi import APIRouter, HTTPException
 
 from app.models.host import Host
@@ -9,19 +11,24 @@ hosts_api = APIRouter()
 
 @hosts_api.post('/api/v1/hosts')
 def add_host(host: Host):
+    """Добавление новой записи хоста."""
 
     new_host_id = host_repo.add_host(host)
     return {'id': new_host_id}
 
 
 @hosts_api.get('/api/v1/hosts')
-def get_all_hosts():
+def get_all_hosts() -> List[Host]:
+    """Извлечение всех записей хостов."""
+
     hosts = host_repo.get_all_hosts()
     return hosts
 
 
 @hosts_api.get('/api/v1/hosts/{host_id}')
-def get_host(host_id: int):
+def get_host(host_id: int) -> Host:
+    """Извлечение записи хоста по его идентификатору."""
+
     try:
         host = host_repo.get_host(host_id)
     except LookupError as err:
@@ -32,6 +39,8 @@ def get_host(host_id: int):
 
 @hosts_api.delete('/api/v1/hosts/{host_id}')
 def delete_host(host_id: int):
+    """Удаление записи хоста с указанными идентификатором."""
+
     try:
         host_repo.delete_host(host_id)
     except LookupError as err:
@@ -41,6 +50,8 @@ def delete_host(host_id: int):
 
 @hosts_api.put('/api/v1/hosts/{host_id}')
 def update_host(host: Host, host_id: int):
+    """Обновление записи хосте."""
+
     try:
         host_repo.update_host(host, host_id)
     except LookupError as err:
--- app/main.py
+++ app/main.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI
 
 from app.api.hosts import hosts_api
 
+APP_DESCRIPTION = """
+API сервиса для дебага других микросервисов.
+"""
 
-app = FastAPI()
+app = FastAPI(description=APP_DESCRIPTION)
 app.include_router(hosts_api)
